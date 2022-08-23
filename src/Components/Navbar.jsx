0436dde1cdcd53da459a59e2ef4bba00283a0108
Pablo <delgadinop@gmail.com>
2022-08-23 20:40:21 -0300

    MenuEcomerce

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <header className="Navbar">
+            <h1 className="Navbar-h1">
+                <a href="/#" className="Navbar-a"> Caelum</a>
+            </h1>
+
+               <nav className= "Navvar-main">
+            <ul className="Navbar-ul">
+                <li className="Navbar-li"><a href="/#" className="Navbar-a">Home</a></li>
+                <li className="Navbar-li"><a href="/#" className="Navbar-a">Quienes Somos</a></li>
+                <li className="Navbar-li"><a href="/#" className="Navbar-a">Catalogo</a></li>
+            </ul>
+        </nav>
+
+
+
+        </header>
+    )
+}
+
+export default Navbar
